30b91e5e25918610b3c06ee1c66845140d96f153
S-boy <sgm1575@hotmail.com>
2016-06-07 15:34:04 +0800

    信息获取正常

--- handle.py
+++ handle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-# _*_ codding = utf-8 _*_
+# _*_ encodding = utf-8 _*_
 # author : k3vi
 # handle HTTP request
 import urllib.request
 import urllib.parse
 
 
-def get(url):
+def get(url):  # 发送get请求，返回网页内容
     user_agent = 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko'
     headers = {"User-Agent": user_agent}
     try:
@@ -18,7 +18,7 @@ def get(url):
         print(e)
 
 
-def post(url):
+def post(url):  # 发送post请求，返回网页内容
     user_agent = "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko"
     try:
         req = urllib.request.Request(url)
@@ -28,3 +28,24 @@ def post(url):
         return page
     except Exception as e:
         print(e)
+
+
+def deal_tags():  # 处理tags.txt，返回一个关键字列表
+    try:
+        with open('./tags.txt', 'rt') as f:
+            tags = f.readlines()
+    except Exception as e:
+        print(e)
+    r_tags = []
+    for tag in tags:
+        tag = to_str(tag)
+        r_tags.append(tag.strip())
+    return r_tags
+
+
+def to_str(bytes_or_str):     # 接受str和bytes，并总返回str
+    if isinstance(bytes_or_str, bytes):
+        value = bytes_or_str.decode('utf-8')
+    else:
+        value = bytes_or_str
+    return value
